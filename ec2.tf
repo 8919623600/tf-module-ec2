0b1e5c696a6d4f027f2904a2a317caed8b7d33f3
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-14 11:27:56 +0530

    auto push

--- ec2.tf
+++ ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "public_instance" {
 resource "aws_instance" "private_instance" {
   ami                     = data.aws_ami.ami.id  # fetching ami id from datasource
   instance_type           = var.instance_type
-#   subnet_id               = aws_subnet.private_subnet.id
+  subnet_id               = var.public_subnet_id
   
   tags = {
     Name = "private-${var.ENV}-server"
