30f59d6f95465989c6a232787d324db650aac7dc
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-14 18:46:01 +0530

    auto push

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "instance_type" {}
 variable "ENV" {}
-variable "pub_subnet_id" {}
-variable "private_subnet_id" {}
+# variable "pub_subnet_id" {}
+# variable "private_subnet_id" {}
 # variable "PUBLIC_SUBNET_CIDR" {}
 variable public_subnet_id {}
