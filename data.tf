587fe400b7bc311c1b10b0948d916e9561fe359d
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-14 20:19:09 +0530

    auto push

---
+++ data.tf
@@ -0,0 +1,11 @@
+# datasource that reads the info from vpc statefile 
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-devops29master"
+    key    = "dev/terrafile/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+#data.terraform_remote_state.vpc.public_subnet.id
