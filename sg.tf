f1c2fefc6e0eb891f41b582777d66b610de661f4
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-16 14:29:10 +0530

    auto push

--- sg.tf
+++ sg.tf
@@ -6,12 +6,9 @@ resource "aws_security_group" "sample_sg" {
 tags = {
     Name = "roboshop-sg"
   }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   
   dynamic ingress {
-    security_group_id = aws_security_group.sample_sg.id
+    
     for_each = var.ingress_rule
     content {
     description     = ingress.value["description"]
@@ -21,12 +18,10 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
     cidr_blocks     = ingress.value["cidr_blocks"]
     }
   }
-}
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+
  
    dynamic egress {
-    security_group_id = aws_security_group.sample_sg.id
     for_each = var.egress_rule
     content {
       description     = egress.value["description"]
@@ -37,5 +32,5 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
     }
    }
 
+
 }
-  
