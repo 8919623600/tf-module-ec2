41f812e0d57ea7aa6cc57d48e9ae064dec59e57b
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-16 12:50:57 +0530

    auto push

---
+++ sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "sample_sg" {
+  name        = "terraform-sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  dynamic ingress {
+    for_each = var.ingress_rule
+    content {
+    description     = ingress.value["description"]
+    from_port       = ingress.value["from_port"]
+    to_port         = ingress.value["to_port"]
+    protocol        = ingress.value["protocol"]
+    cidr_blocks     = ingress.value["cidr_blocks"]
+    }
+  }
+   dynamic egress {
+    for_each = var.egress_rule
+    content {
+      description     = egress.value["description"]
+      from_port       = egress.value["from_port"]
+      to_port         = egress.value["to_port"]
+      protocol        = egress.value["protocol"]
+      cidr_blocks     = egress.value["cidr_blocks"]
+    }
+   }
+
+
+  tags = {
+    Name = "terraform-sg"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -4,3 +4,35 @@ variable "ENV" {}
 # variable "private_subnet_id" {}
 # variable "PUBLIC_SUBNET_CIDR" {}
 # variable public_subnet_id {}
+variable  "ingress_rule" {
+    default = [
+    {
+      description     = "ssh"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  },
+    {
+      description     = "web"
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  },
+     {
+      description     = "jenkins"
+      from_port       = 8080
+      to_port         = 8080
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  },
+     {
+      description     = "nexus"
+      from_port       = 8081
+      to_port         = 8081
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+  }
+    ]
+} 
